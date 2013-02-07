d4a75214fa9f4ac4d548676f478d3b8e8ad63d1e
2rgeir@gmail.com <2rgeir@gmail.com@04e56d38-e186-5764-2453-bb73e5fb35f8>
2013-02-07 10:25:11 +0000

    Gjort litt av koden mer oversiktlig og lagt til ett par kommentarer

--- trunk/WindowsGame1/WindowsGame1/WindowsGame1/Map1.cs
+++ trunk/WindowsGame1/WindowsGame1/WindowsGame1/Map1.cs
@@ -11,9 +11,6 @@ namespace WindowsGame1
 {
     class Map1
     {
-        public List<Vector2> Ground;
-        public List<Vector2> Wall;
-        Texture2D Black;
         Backgrounds splitter = new Backgrounds();
         Objects staticObjects = new Objects();
         Objects foregroundObjects = new Objects();
@@ -51,15 +48,9 @@ namespace WindowsGame1
             
         }
 
-        public void Initialize(Texture2D Black, ContentManager Content, int ScreenWidth, int ScreenHeight)
+        public void Initialize(ContentManager Content, int ScreenWidth, int ScreenHeight)
         {
-            this.Black = Black;
-            int StartX=50;
-            int StartY=600;
-            Ground = new List<Vector2>();
-            Wall = new List<Vector2>();
-
-            //vineWall = Content.Load<Texture2D>("vine-wall_204x320");
+            
             splitter.Initialize(Content, "Foreground_tutorial/TutorialMap-foreground__", ScreenWidth, 0, 16);
             staticObjects.addObject(Content, "Elements/interactive/sheet/object__01", 1240, 285, ScreenWidth, ScreenHeight, 204, 320, 100, 10);
             staticObjects.addObject(Content, "Elements/interactive/sheet/object__03", 1560, 4400, ScreenWidth, ScreenHeight, 410, 529, 100, 1);
@@ -72,20 +63,7 @@ namespace WindowsGame1
             staticObjects.addObjectType2(Content, paths, 1580, 2170, ScreenWidth, ScreenHeight, 932, 251, 100, 10);
             addToStringList("Elements/interactive/sheet/toxic-pool/toxic-pool_", 6);
             staticObjects.addObjectType2(Content, paths, 2800, 4370, ScreenWidth, ScreenHeight, 848, 280, 150, 6);
-            for (int i = 0; i < 100; i++)
-            {
-                Ground.Add(new Vector2(StartX, StartY));
-                StartX += 5;
-                //StartY -= 5;
-            }
-            StartX = 50;
-            StartY = 350;
-            for (int i = 0; i < 100; i++)
-            {
-                Ground.Add(new Vector2(StartX, StartY));
-                StartX += 5;
-                //StartY -= 5;
-            }
+
 
 
             //StartX = 300;
